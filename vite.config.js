e64754e2801617c384b603299c3ff1f93e333821
Macha-Akhil <118301122+Macha-Akhil@users.noreply.github.com>
2025-02-18 15:34:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist", // Make sure this matches your workflow
+  },
   plugins: [react()],
   server: {
     proxy: {
